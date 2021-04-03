c0aa646f6af705431092ad804b29374c39635191
Роман Г. Хрусталев <khrustalev@yoomoney.ru>
2021-04-03 13:04:18 +0300

    fix static

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "statics")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
